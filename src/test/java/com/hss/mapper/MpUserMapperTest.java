c30a3fcdf7cbe10844caabb094b6dd7ae57f0ce5
hss1552 <hesensen@tzt.cn>
2024-08-27 17:17:07 +0800

    mp写法优化

--- src/test/java/com/hss/mapper/MpUserMapperTest.java
+++ src/test/java/com/hss/mapper/MpUserMapperTest.java
@@ -10,11 +10,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import javax.annotation.Resource;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,7 +22,7 @@ public class MpUserMapperTest {
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Autowired
+    @Resource
     private MpUserMapper mpUserMapper;
 
     @Test
